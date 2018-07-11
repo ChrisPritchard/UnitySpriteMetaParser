ba32df95db1174d46b119b329325427e123d1bd0
Chris Pritchard <chris.pritchard@edpay.nz>
2018-07-12 10:21:08 +1200

    Fixed bugs

--- Program.fs
+++ Program.fs
@@ -1,12 +1,12 @@
 ﻿
-[<EntryPoint>]
-let main argv =
-    let (all, _) = 
-        System.IO.File.ReadAllLines "./Knight.png.meta"
+open System.IO
+
+let getInfo path = 
+    System.IO.File.ReadAllLines path
         |> Seq.fold (fun (all, current) line -> 
             let clean = line.Trim().Replace("\t", "")
             match current with
-            | Some (name, x, y, w, h) -> 
+            | Some (name, x, y, w, _) -> 
                 if clean.StartsWith("x: ") then
                     let newSet = name, int <| clean.Substring("x: ".Length), -1, -1, -1
                     (all, Some newSet)
@@ -27,7 +27,24 @@ let main argv =
                     let newSet = name, -1, -1, -1, -1
                     (all, Some newSet)
                 else
-                    (all, current)) ([], None)
-    let text = sprintf "name,x,y,width,height\r\n%s" <| (all |> Seq.rev |> Seq.map (fun (n, x, y, w, h) -> sprintf "%s,%i,%i,%i,%i" n x y w h) |> String.concat "\r\n")
-    System.IO.File.WriteAllText ("./result.csv", text)
-    0
+                    (all, current)) ([], None) |> fst
+
+[<EntryPoint>]
+let main argv =
+    let (path, out) =
+        match argv with
+        | [|inPath|] -> inPath,"./output.csv"
+        | [|inPath;outPath|] -> inPath,outPath
+        | _ -> "",""
+    if path = "" || File.Exists path |> not then
+        printfn "A valid input file path was not specified"
+        -1
+    else
+        let info = getInfo path
+        if Seq.isEmpty info then
+            printfn "File specified did not contain any image info"
+            -2
+        else
+            let text = sprintf "name,x,y,width,height\r\n%s" <| (info |> Seq.rev |> Seq.map (fun (n, x, y, w, h) -> sprintf "%s,%i,%i,%i,%i" n x y w h) |> String.concat "\r\n")
+            File.WriteAllText ("./result.csv", text)
+            0
